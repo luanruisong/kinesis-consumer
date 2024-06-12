0b0e07c0e411623a39475b386abd39c04494f0e3
Luan Ruisong <luanruisong@bituniverse.org>
2024-06-13 02:07:39 +0800

    fix package name

--- examples/consumer-postgres/main.go
+++ examples/consumer-postgres/main.go
@@ -13,8 +13,8 @@ import (
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/credentials"
 	"github.com/aws/aws-sdk-go-v2/service/kinesis"
-	store "github.com/harlow/kinesis-consumer/store/postgres"
 	consumer "github.com/luanruisong/kinesis-consumer"
+	store "github.com/luanruisong/kinesis-consumer/store/postgres"
 )
 
 func main() {
